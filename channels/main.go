11f6243ad9471cbceae0cd2204e1f2c88d33fdfc
Alex Alegre <aalegre@salucro.com>
2018-07-24 17:38:20 -0700

    add channels to know when a response has been received

--- channels/main.go
+++ channels/main.go
@@ -7,25 +7,32 @@ import (
 
 func main() {
 	links := []string{
-		"http://google.com",
+		"http://golang.org",
 		"http://facebook.com",
 		"http://stackoverflow.com",
 		"http://amazon.com",
-		"http://golang.org",
+		"http://google.com",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link, c)
+	}
+
+	for i := 0; i < len(links); i++ {
+		fmt.Println(<-c)
 	}
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
-
 	if err != nil {
 		fmt.Printf("%v might be down \n", link)
+		c <- "Might be down I think"
 		return
 	}
 
 	fmt.Printf("%v is up! \n", link)
+	c <- "Yep, it's up"
 }
