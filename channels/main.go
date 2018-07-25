ae3d815003e67b4c11e0d1c524c5e299093f857e
Alex Alegre <aalegre@salucro.com>
2018-07-24 17:43:22 -0700

    ping sites until one fails

--- channels/main.go
+++ channels/main.go
@@ -20,8 +20,8 @@ func main() {
 		go checkLink(link, c)
 	}
 
-	for i := 0; i < len(links); i++ {
-		fmt.Println(<-c)
+	for {
+		go checkLink(<-c, c)
 	}
 }
 
@@ -29,10 +29,10 @@ func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Printf("%v might be down \n", link)
-		c <- "Might be down I think"
+		c <- link
 		return
 	}
 
 	fmt.Printf("%v is up! \n", link)
-	c <- "Yep, it's up"
+	c <- link
 }
